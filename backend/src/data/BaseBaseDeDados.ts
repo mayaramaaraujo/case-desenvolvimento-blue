9c9e8df60044e8f90fc4246f7c1229f46df4f6ba
Mayara Araujo <mayaramaaraujo98@gmail.com>
2021-01-17 10:05:36 -0400

    alteração da porta db

--- backend/src/data/BaseBaseDeDados.ts
+++ backend/src/data/BaseBaseDeDados.ts
@@ -14,12 +14,13 @@ export default class BaseBaseDeDados {
         client: "mysql",
         connection: {
             host: process.env.DB_HOST,
-            port: 3306,
+            port: 3307,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME
         }
     })
+
     static connection: any
 
     public static async destroyConnection(): Promise<void> {
