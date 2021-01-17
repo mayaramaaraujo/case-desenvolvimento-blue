018aefaad2b090f42e3081d5dc6192c1d36e8444
Mayara Araujo <mayaramaaraujo98@gmail.com>
2021-01-17 10:11:00 -0400

    alteração da porta db

--- backend/src/data/BaseBaseDeDados.ts
+++ backend/src/data/BaseBaseDeDados.ts
@@ -14,7 +14,7 @@ export default class BaseBaseDeDados {
         client: "mysql",
         connection: {
             host: process.env.DB_HOST,
-            port: 3307,
+            port: 3306,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME
